f8c518943922bb8921e2680b55b5fd5620d7eb2a
Fran Iglesias <franiglesias@mac.com>
2022-07-18 13:29:26 +0200

    First commit

---
+++ src/test/java/com/codurance/password/PasswordValidatorShould.java
@@ -0,0 +1,22 @@
+package com.codurance.password;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/*
+* Have more than 8 characters
+* Contains a capital letter
+* Contains a lowercase
+* Contains a number
+* Contains an underscore
+* */
+public class PasswordValidatorShould {
+
+    @Test
+    void doSomething() {
+        assertFalse(true);
+    }
+}
